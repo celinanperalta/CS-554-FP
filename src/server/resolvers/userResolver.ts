372de2b4dec736860d12e286ea66a7e4c6a58bd9
Kai Engwall <kengwall8@gmail.com>
2021-12-02 18:47:17 -0500

    added ability to update users,prompts,comments

--- src/server/resolvers/userResolver.ts
+++ src/server/resolvers/userResolver.ts
@@ -21,4 +21,33 @@ export class UserResolver {
         return await userService.addUser(username,email,hashedPassword);
     }
 
+    @Mutation(returns => User, {nullable: true})
+    async updateUser(@Arg("id")id: string,
+    @Arg("username", {nullable:true})username?: string,
+    @Arg("email", {nullable:true})email?: string,
+    @Arg("hashedPassword", {nullable:true})hashedPassword?: string,
+    @Arg("prompts", (type)=>[String], {nullable:true})prompts?: string[],
+    @Arg("accessToken", {nullable:true})accessToken?: string,
+    @Arg("refreshToken", {nullable:true})refreshToken?: string,
+    @Arg("profile_picture", {nullable:true})profile_picture?: string,
+    @Arg("likes", (type)=>[String], {nullable:true})likes?: string[],
+    @Arg("votes", (type)=>[String], {nullable:true})votes?: string[],
+    @Arg("submissions", (type)=>[String], {nullable:true}) submissions?: string[],
+    @Arg("comments", (type)=>[String], {nullable:true}) comments?: string[]
+    ) : Promise<User>{
+        return await userService.updateUser({id: id,
+            username: username,
+            email: email,
+            password: hashedPassword,
+            prompts: prompts,
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            profile_picture: profile_picture,
+            likes: likes,
+            votes: votes,
+            submissions: submissions,
+            comments:comments
+        });
+    }
+
 }
--- src/server/services/promptService.ts
+++ src/server/services/promptService.ts
@@ -1,7 +1,8 @@
 import client from "../config/esConnection";
 import { Prompt } from "../schemas/Prompt";
 import {v4 as uuid} from 'uuid';
-
+import { promptPatch } from "../config/types";
+import userService from "./userService";
 
 const getPrompts = async () : Promise<Prompt[]> => {
     const {body} = await client.search({
@@ -35,16 +36,46 @@ const addPrompt = async ( prompt: string,  posted_by: string,  submittedSongs: s
     new_prompt.prompt = prompt
     new_prompt.posted_by = posted_by
     new_prompt.comments = comments
+    new_prompt.submittedSongs = submittedSongs;
     await client.index({
-        index: 'comments',
+        index: 'prompts',
         id: new_prompt.id,
         body: new_prompt,
     })
+
+
+    //updating user's prompt id array to hold this as well
+    let user = await userService.getUserById(posted_by);
+    user.prompts = [...user.prompts, new_prompt.id];
+    console.log(user.prompts);
+    await userService.updateUser(user);
+
     return new_prompt;
 }
+
+const updatePrompt = async ( newPromptInfo : promptPatch) : Promise<Prompt> => {
+    if(!newPromptInfo.id || !newPromptInfo.posted_by){
+        throw "Must provide prompt ID and userposted to update prompt";
+    }
+   console.log(newPromptInfo);
+   let oldPrompt = await getPromptById(newPromptInfo.id);
+   let newPrompt : Prompt = {...oldPrompt};
+   if(newPromptInfo.comments){newPrompt.comments=newPromptInfo.comments};
+   if(newPromptInfo.prompt){newPrompt.prompt=newPromptInfo.prompt};
+   if(newPromptInfo.submittedSongs){newPrompt.submittedSongs=newPromptInfo.submittedSongs};
+   await client.update({
+    index: 'prompts',
+    id: newPrompt.id,
+    body: {doc:newPrompt}
+    });
+
+    return newPrompt;   
+}
+
 
 export default {
     getPrompts,
     getPromptById,
-    addPrompt
+    addPrompt,
+    updatePrompt
 }
