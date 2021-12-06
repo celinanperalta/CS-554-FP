eda64e9f8084b4520a3590e323550ee809eec214
kaiqichee <55299653+kaiqichee@users.noreply.github.com>
2021-12-06 18:38:08 -0500

    added front end form

--- src/client/pages/login.tsx
+++ src/client/pages/login.tsx
@@ -4,6 +4,35 @@ const  Login = () => {
   return (
     <div className= "app">
       <h2>Login</h2>
+      <form
+      method="POST"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      name="loginForm"
+      // className="center"
+    >
+      <label>
+        <span>Username: </span>
+        <input
+          autoComplete="off"
+          type="text"
+          name="username"
+          // onChange={login}
+        />
+      </label>
+      <br />
+      <label>
+        <span>Password: </span>
+        <input
+          autoComplete="off"
+          type="text"
+          name="password"
+          // onChange={login}
+        />
+      </label>
+    </form>
+    <button type="submit">Login</button>
     </div>
   )
 }
