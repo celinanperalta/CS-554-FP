d2a97fffa85bbf02db9e87c60f3fbcaa43d32001
Celina Peralta <celinanperalta@gmail.com>
2021-12-10 13:54:28 -0500

    implement login + persist

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,9 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
-import express, * as Express from "express";
+import * as Express from "express";
 import { buildSchema } from "type-graphql";
 import { v4 as uuid } from "uuid";
-import { GraphQLLocalStrategy, buildContext } from "graphql-passport";
+import { buildContext } from "graphql-passport";
 
 import { SongResolver } from "./resolvers/songResolver";
 import { UserResolver } from "./resolvers/userResolver";
@@ -14,18 +14,21 @@ import { SongSubmissionResolver } from "./resolvers/songSubmissionResolver";
 
 import { User } from "./schemas/User";
 
-import userService from "./services/userService";
 import { seed } from "./config/esConnection";
 import initPassport from "./config/initPassport";
-import { emitWarning } from "process";
+import * as passport from "passport";
 
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
-const passport = require("passport");
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+  origin: [
+    "http://localhost:3000",
+    "https://studio.apollographql.com",
+    "http://localhost:4000",
+    "https://accounts.spotify.com/authorize",
+  ],
   credentials: true,
 };
 
@@ -36,47 +39,62 @@ async function main() {
   require("dotenv").config();
 
   const schema = await buildSchema({
-    resolvers: [SongResolver, UserResolver, AuthResolver, CommentResolver, PromptResolver, SongSubmissionResolver],
+    resolvers: [
+      SongResolver,
+      UserResolver,
+      AuthResolver,
+      CommentResolver,
+      PromptResolver,
+      SongSubmissionResolver,
+    ],
     emitSchemaFile: true,
   });
 
-  initPassport();
-
   const app = Express();
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.set(
+      "Cache-Control",
+      "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+    );
+
+    next();
+  });
 
   app.use(cors(corsOptions));
 
   app.use(
     session({
-      genid: (req: any) => uuid(),
+      genid: (req) => uuid(),
       secret: SESSION_SECRET,
       resave: false,
-      saveUninitialized: true,
-      cookie: {
-        httpOnly: true,
-        secure: true,
-      },
+      saveUninitialized: false,
     })
   );
 
-  app.use(passport.initialize());
-  app.use(passport.session());
+  initPassport(app);
 
   app.get(
     "/auth/spotify",
     passport.authenticate("spotify", {
-      scope: ['user-read-email', 'user-read-private'],
-      showDialog: false,
+      scope: ["user-read-email", "user-read-private"],
     })
   );
 
   app.get(
     "/auth/spotify/callback",
-    passport.authenticate("spotify", { failureRedirect: "/login" }),
+    passport.authenticate("spotify", {
+      failureRedirect: "http://localhost:3000/",
+    }),
     function (req, res) {
       // Successful authentication, redirect home.
       console.log(req.user);
-      res.redirect("/");
+      res.redirect("http://localhost:3000/users/me");
     }
   );
 
