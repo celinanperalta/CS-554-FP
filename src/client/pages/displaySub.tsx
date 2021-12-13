6c50d8dda78e563b82fb053052984f80fc343664
kaiqichee <55299653+kaiqichee@users.noreply.github.com>
2021-12-13 11:04:28 -0500

    added cards for active submission and display subission
    components for user dashboard/home?

---
+++ src/client/pages/displaySub.tsx
@@ -0,0 +1,85 @@
+//I pledge my honor that I have abided by the Stevens Honor System.
+// import '../App.css';
+// import noImage from '../noImage.jpg';
+import React, {useState, useEffect}  from "react";
+import Link from "next/link";
+import Like from '@material-ui/icons/FavoriteBorder';
+import Comment from '@material-ui/icons/ChatBubbleOutline';
+
+import {
+    Card,
+    CardContent,
+    CardMedia,
+    Grid,
+    Typography,
+    makeStyles,
+    CardHeader,
+    Button,
+    IconButton
+  } from '@material-ui/core';
+  
+const useStyles = makeStyles({
+    card: {
+      maxWidth: 250,
+      height: 'auto',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      borderRadius: 5,
+      border: '1px solid #1e8678',
+      textAlign: "left",
+      boxShadow: '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);'
+    },
+    titleHead: {
+      borderBottom: '1px solid #1e8678',
+      fontWeight: 'bold'
+    },
+    grid: {
+      flexGrow: 1,
+      flexDirection: 'row'
+    },
+    media: {
+      height: '100%',
+      width: '100%'
+    },
+    header: {
+        backgroundColor: '#333',
+        color: "#FFFFFF"
+    },
+    button: {
+      color: '#04AA6D',
+      borderColor: '#04AA6D',
+      margin: "10px"
+    },
+    // 'button:hover': {
+    //     backgroundColor: '#333',
+    //     borderColor: '#333',
+    //     boxShadow: 'none',
+    //   }
+  });
+
+const DisplaySub = (props) => {
+    const classes = useStyles();
+    const [song, setSong] = useState(undefined);
+
+    return (
+        <Grid item className={classes.grid} xs={12} sm={6} md={4} lg={3} xl={2}>
+            <Card className={classes.card} variant="outlined">
+                <Link href={`/prompts/${props.data.id}`}>
+                    <CardHeader className={classes.header} subheader={props.data.prompt} />
+                </Link>
+                <CardContent>
+                    song data
+                </CardContent>
+                <IconButton color="" aria-label="like prompt" component="span">
+                    <Like />
+                </IconButton>
+                <IconButton color="" aria-label="comment on prompt" component="span">
+                    <Comment />
+                </IconButton>
+                <br/>                          
+            </Card>
+        </Grid>
+    )
+  }
+
+export default DisplaySub;
