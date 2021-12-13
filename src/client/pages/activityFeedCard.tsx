7cb00ad5998412c765679424d2c007cd57aa2d2d
kaiqichee <55299653+kaiqichee@users.noreply.github.com>
2021-12-13 16:42:51 -0500

    added activity feed components

---
+++ src/client/pages/activityFeedCard.tsx
@@ -0,0 +1,73 @@
+//I pledge my honor that I have abided by the Stevens Honor System.
+// import '../App.css';
+// import noImage from '../noImage.jpg';
+import React, {useState, useEffect}  from "react";
+import Link from "next/link";
+
+import {
+    Card,
+    CardContent,
+    CardMedia,
+    Grid,
+    Typography,
+    makeStyles,
+    CardHeader,
+    Button,
+    IconButton
+  } from '@material-ui/core';
+  
+const useStyles = makeStyles({
+    card: {
+      maxWidth: 250,
+      minWidth: 250,
+      height: 'auto',
+      margin: '10px',
+      borderRadius: 5,
+      border: '1px solid',
+      textAlign: "left"
+    },
+    titleHead: {
+      borderBottom: '1px solid #1e8678',
+      fontWeight: 'bold'
+    },
+    grid: {
+      flexGrow: 1,
+      flexDirection: 'row'
+    },
+    media: {
+      height: '100%',
+      width: '100%'
+    },
+    header: {
+        backgroundColor: '#333',
+        //color: 'white'
+    },
+    button: {
+      color: '#04AA6D',
+      borderColor: '#04AA6D',
+      margin: "10px"
+    },
+    // 'button:hover': {
+    //     backgroundColor: '#333',
+    //     borderColor: '#333',
+    //     boxShadow: 'none',
+    //   }
+  });
+
+const ActivityFeedCard = (props) => {
+    const classes = useStyles();
+
+    return (
+        <Grid item className={classes.grid} xs={12} sm={6} md={4} lg={3} xl={2}>
+            <Card className={classes.card} variant="outlined">
+                <Link href={`/prompts/${props.data.id}`}>
+                <CardContent>
+                    <Typography className="promptContent" ><b>{props.data.action}: </b>"{props.data.prompt}"</Typography>
+                </CardContent>
+                </Link>
+            </Card>
+        </Grid>
+    )
+  }
+
+export default ActivityFeedCard;
--- src/client/pages/login.tsx
+++ src/client/pages/login.tsx
@@ -3,10 +3,14 @@ import ActivePrompt from "./activePrompt";
 import DisplaySubmission from "./displaySub";
 import TopSong from "./topSongCard";
 import Profile from "./userProfile";
+import ActivityFeedCard from "./activityFeedCard";
+import ActivityFeed from "./activityFeed";
 
 const  Login = () => {
 
   let item = {id: 12, prompt: "songs thats play when you're on your last final", posted_by: "User3"}
+  let activity = {action: "new submission", prompt: "songs thats play when you're on your last final"}
+
     
 
   return (
@@ -45,6 +49,8 @@ const  Login = () => {
     <DisplaySubmission data={item} />
     <TopSong data={item} />
     <Profile data={item} />
+    <ActivityFeedCard data={activity} />
+    <ActivityFeed />
     <br />
     <br />
     </div>
