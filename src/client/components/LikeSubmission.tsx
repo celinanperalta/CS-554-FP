8f7dcaf74bf13a6979a4cd128db6acf4ff69d751
Celina Peralta <celinanperalta@gmail.com>
2021-12-16 16:25:37 -0500

    like button

---
+++ src/client/components/LikeSubmission.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useMutation } from "@apollo/client";
+import queries from "../queries";
+import Like from "@material-ui/icons/FavoriteBorder";
+import { IconButton } from "@material-ui/core";
+
+const LikeSubmission = ({ submission }) => {
+
+  const [likeSubmission] = useMutation(queries.ADD_SONG_SUB_VOTE, {
+    variables: {
+      submissionId: submission.id,
+    },
+    refetchQueries: [
+        {
+            query: queries.GET_SONG_SUB,
+            variables: {
+                id: submission.id,
+            },
+        },
+    ],
+  });
+
+  const handleLike = () => {
+    likeSubmission();
+  };
+
+  return (
+    <IconButton onClick={handleLike}>
+      <Like />
+    </IconButton>
+  );
+};
+
+export default LikeSubmission;
