f733f652f669b5d62205eb5e22a241ac289412e7
kaiqichee <55299653+kaiqichee@users.noreply.github.com>
2021-12-16 21:12:15 -0500

    add mutation to delete a comment
    also made it so the prompt text links to the prompt page instead of the whole card

--- src/client/components/Comment.tsx
+++ src/client/components/Comment.tsx
@@ -1,5 +1,7 @@
 import queries from "../queries";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
+import Close from '@material-ui/icons/Close';
+import Like from '@material-ui/icons/FavoriteBorder';
 
 import {
   Card,
@@ -44,19 +46,51 @@ const useStyles = makeStyles({
     borderColor: "#04AA6D",
     margin: "10px",
   },
-  // 'button:hover': {
-  //     backgroundColor: '#333',
-  //     borderColor: '#333',
-  //     boxShadow: 'none',
-  //   }
+  close: {
+    display: 'flex',
+    padding: '0px',
+    margin: '0px',
+    justifyContent: 'right',
+    marginTop: '5px',
+    marginRight: '7px',
+  },
+  icon: {
+    padding: '0px',
+    margin: '0px'
+  },
+  status: {
+        display: 'flex',
+        flexDirection: 'row',
+        padding: '0px',
+        margin: '0px',
+        justifyContent: 'left',
+        marginTop: '5px'
+    },
+    values: {
+        padding: '0px',
+        margin: '0px',
+        paddingLeft: '10px',
+        paddingRight: '10px'
+    },
 });
 
 const Comment = (props) => {
   const classes = useStyles();
+  const [deleteComment] = useMutation(queries.DELETE_COMMENT);
+
   const { loading, error, data } = useQuery(queries.GET_COMMENT, {
     variables: { commentId: props.commentId },
     pollInterval: 10000,
   });
+
+  const handleDelete = () => {
+    console.log("deleting now")
+    console.log();
+    deleteComment({
+      variables: {id: data.getCommentById.id}
+    });
+    console.log("deleted done")
+  }
 
   if (loading) {
     return (
@@ -65,9 +99,15 @@ const Comment = (props) => {
       </div>
     );
   }
+
   return (
     <Grid item className={classes.grid} xs={12} sm={6} md={4} lg={3} xl={2}>
       <Card className={classes.card} variant="outlined">
+        <div className={classes.close}>
+          <IconButton className={classes.icon} onClick={handleDelete} color="default" aria-label="like prompt" component="span">
+            <Close />
+          </IconButton>
+        </div>
         <CardContent>
           <Typography className="promptContent">
             {data.getCommentById.comment}
@@ -75,7 +115,11 @@ const Comment = (props) => {
           <Typography>
             {data.getCommentById.posted_by} 
           </Typography>
-          {data.getCommentById.likes.length} likes
+          <div className={classes.status}>
+              <Like />
+              {console.log(data.getCommentById)}
+              <p className={classes.values}> {data.getCommentById.likes.length}</p> 
+          </div>
         </CardContent>
       </Card>
     </Grid>
--- src/client/queries.ts
+++ src/client/queries.ts
@@ -197,6 +197,18 @@ mutation Mutation($comment: String!, $promptId: String!) {
     likes
   }
 }`
+
+const DELETE_COMMENT = gql`
+mutation Mutation($id: String!) {
+  deleteComment(id: $id) {
+    id
+    prompt_id
+    comment
+    posted_by
+    likes
+  }
+}`
+
 const UPDATE_USER = gql`
 mutation UpdateUser($id: String!, $username: String, $email: String, $password: String) {
   updateUser(id: $id, username: $username, email: $email, password: $password) {
@@ -222,5 +234,6 @@ export default {
   ADD_COMMENT,
   GET_SONG_SUBS,
   ADD_PROMPT,
-  UPDATE_USER
+  UPDATE_USER,
+  DELETE_COMMENT
 };
